acb60af36dafcbebd2629e6792471699714e027e
Eric Bower <neurosnap@gmail.com>
2018-07-12 16:04:55 -0400

    typescript update

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'cosed' {
   export type CoFn = Iterator<any>;
   export type TaskFn = (fn: CoFn, ...args: any[]) => Promise<any>;
-  export type Fn = (...args: any[]) => void | Iterator<any>;
+  export type Fn = (...args: any[]) => any;
   export interface CallEffect {
     type: 'CALL';
     fn: Fn;
