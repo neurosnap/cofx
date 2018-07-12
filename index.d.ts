b2af379728c7eef4ab76f08aa5676119e08e4dba
Eric Bower <neurosnap@gmail.com>
2018-07-12 00:00:48 -0400

    add typings

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+declare module 'cosed' {
+  export type CoFn = Iterator<any>;
+  export type Fn = (...args: any[]) => void | Iterator<any>;
+  export interface CallEffect {
+    type: 'CALL';
+    fn: Fn;
+    args: any[];
+  }
+  export interface SpawnEffect {
+    type: 'SPAWN';
+    fn: Fn;
+    args: any[];
+  }
+  export interface DelayEffect {
+    type: 'DELAY';
+    ms: number;
+  }
+  export interface AllEffect {
+    type: 'ALL';
+    effects: Effect[];
+  }
+  export type Effect = { type: string } & { [key: string]: any };
+  type NextFn = () => Middleware;
+  type Promisify = (p: any) => Promise<any>;
+  type Middleware = (next: NextFn) => (effect: Effect, promisify: Promisify) => Middleware;
+
+  export function task(fn: CoFn): Promise<any>;
+  export function call(fn: Fn, ...args: any[]): CallEffect;
+  export function all(effects: Effect[]): AllEffect;
+  export function delay(ms: number): DelayEffect;
+  export function factory(...args: Middleware[]): (fn: CoFn) => Promise<any>;
+  export function factoryBase(...args: Middleware[]): any;
+  export function spawn(fn: Fn, ...args: any[]): any;
+}
